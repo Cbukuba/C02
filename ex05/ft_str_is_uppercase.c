4769162ffa9f250c79beec267c1f619524a1581b
cbukuba <cbukuba@student.s19.be>
2021-09-15 13:01:27 +0000

    Exercise05

---
+++ ex05/ft_str_is_uppercase.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 12:56:26 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/09/15 12:56:30 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			i ++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/*int	main()
+{
+	int reseult;
+	reseult = ft_str_is_uppercase("H$H");
+	printf("%d", reseult);
+}*/
