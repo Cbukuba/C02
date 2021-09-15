58944abf40a0c0a6c79816dd64551a774569e20f
cbukuba <cbukuba@student.s19.be>
2021-09-15 17:52:15 +0000

    Exercise09

---
+++ ex09/ft_strcapitalize.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 14:05:10 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/09/15 14:05:13 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] != '\0' && str[0] >= 97 && str[0] <= 127)
+		str[0] = str[0] - 32 ;
+	while (str[i] != '\0')
+	{
+		if ((str[i - 1] >= 32 && str[i - 1] <= 47)
+			&& (str[i] >= 97 && str[i] <= 122))
+			str[i] = str[i] - 32;
+		if ((str[i - 1] >= 65 && str[i - 1] <= 90)
+			&& (str[i] >= 65 && str[i] <= 90 ))
+			str[i] = str[i] + 32;
+		if ((str[i - 1] >= 48 && str[i - 1] <= 57)
+			&& (str[i - 1] >= 97 && str[i - 1] <= 122))
+			str[i] = str[i] - 32;
+		i ++;
+	}
+	return (str);
+}
+
+/*int main()
+{
+    char str [] = "salut, comment tu vas ? 
+		42mots quarante-deux; cinquante+et+un";
+    printf("%s",ft_strcapitalize(str));
+}*/
